42c74e8da212394257b32ff0787a1560982cf466
1812425414 <1812425414@qq.com>
2020-04-03 16:45:39 +0800

    8

---
+++ src/leetcode/T8.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @Classname T8
+ * @Description TODO leetCode第8题 字符串转换整数
+ * @Date 2020/4/3 13:16
+ * @Author ALexNi
+ */
+public class T8 {
+    public static void main(String[] args) {
+        String s = "   123";
+        System.out.println(myAtoi(s));
+    }
+
+    public static int myAtoi(String string){
+        String pattern = "[-+]??[0-9]++";
+        Pattern r = Pattern.compile(pattern);
+        Matcher matcher = r.matcher(string);
+        if (matcher.lookingAt()) {
+            String strNum = string.substring(0, matcher.end());
+            // 如果直接转32位int出现NFE那么就只要判断是Integer的最大值还是最小值就好了
+            try {
+                return Integer.parseInt(strNum);
+            } catch (NumberFormatException nfe) {
+                return strNum.charAt(0) == '-' ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            }
+        }
+        return 0;
+    }
+}
--- src/te.java
+++ src/te.java
@@ -1,18 +1,26 @@
-import java.util.*;
-import java.util.concurrent.ThreadPoolExecutor;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class te {
-    public static void main(String[] args) {
-        Map<Character, Integer> map = new HashMap<>(5);
-        char i_ch='0';
-        for (int i = 0; i < 5; i++){
-            i_ch += i;
-            map.put(i_ch,i);
-        }
-        for (Map.Entry entry:map.entrySet()){
-            if ((Integer)entry.getValue()>1){
-                System.out.println(entry.getKey().toString());
-            }
-        }
+    private static final String REGEX = "foo";
+    private static final String INPUT = "fooooooooooooooooo";
+    private static final String INPUT2 = "ooooofoooooooooooo";
+    private static Pattern pattern;
+    private static Matcher matcher;
+    private static Matcher matcher2;
+
+    public static void main( String args[] ){
+        pattern = Pattern.compile(REGEX);
+        matcher = pattern.matcher(INPUT);
+        matcher2 = pattern.matcher(INPUT2);
+
+        System.out.println("Current REGEX is: "+REGEX);
+        System.out.println("Current INPUT is: "+INPUT);
+        System.out.println("Current INPUT2 is: "+INPUT2);
+
+
+        System.out.println("lookingAt(): "+matcher.lookingAt());
+        System.out.println("matches(): "+matcher.matches());
+        System.out.println("lookingAt(): "+matcher2.lookingAt());
     }
 }
